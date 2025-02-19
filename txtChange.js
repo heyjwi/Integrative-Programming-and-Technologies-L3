8c4b599dc3db2479cebfe72865fe1def30f07dba
heyjwi <heji042918@gmail.com>
2025-02-20 02:36:45 +0800

    First Commit

--- txtChange.js
+++ txtChange.js
@@ -1,3 +1,8 @@
 function changeText() {
+
     document.getElementById("message").innerText = "ANO GOTO NG BEBE KO?🥹\nLIMAAA5️⃣\nLIMAAAA?😩🖐\n👲🏻WOMANJING SAMSURING SHI SAMSAM👲🏻";
+    
+
+    let audio = new Audio('womanjing.mp3');
+    audio.play();
 }
